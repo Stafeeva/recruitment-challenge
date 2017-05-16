8e7c8ffa2a1702c8ea163b7a8204a5183d5c988d
Natalia Stafeeva <n.stafeeva@gmail.com>
2017-05-16 14:38:05 +0100

    Refactor app.get /clients

--- server.js
+++ server.js
@@ -12,23 +12,11 @@ app.get('/', (req, res) => {
 })
 
 app.get('/clients', (req, res) => {
-  const allClients = clients.Clients
-  const printHTML = []
-  allClients.forEach((client) => {
-    printHTML.push("<h3><a href='/clients/" + client.name + "'>" + client.name + "</a></h3>");
-  })
-  res.send(printHTML.join(''));
+  res.send(printListOfClientsAsHTML());
 })
 
 const googleMapAPIUrl = "https://maps.googleapis.com/maps/api/distancematrix/json?origins=B47XG&destinations=B421QZ|B691EQ&key=" + APIKey
 var matrix = null
-
-request(googleMapAPIUrl, function (error, response, body) {
-  console.log('error:', error);
-  console.log('statusCode:', response && response.statusCode);
-  matrix = JSON.parse(body);
-  console.log(matrix)
-});
 
 app.get('/clients/:clientName', (req, res) => {
   const clientName = req.params.clientName
@@ -39,11 +27,27 @@ app.listen(3000, () => {
   console.log('Go to  localhost:3000!')
 })
 
+printListOfClientsAsHTML = () => {
+  const allClients = clients.Clients
+  const printHTML = []
+  allClients.forEach((client) => {
+    printHTML.push("<h3><a href='/clients/" + client.name + "'>" + client.name + "</a></h3>");
+  })
+  return printHTML.join('')
+}
+
+request(googleMapAPIUrl, function (error, response, body) {
+  console.log('error:', error);
+  console.log('statusCode:', response && response.statusCode);
+  matrix = JSON.parse(body);
+  console.log(matrix)
+});
+
 printMatrixAsHTML = (clientName, matrix) => {
   var printHTML = "<h3>" + clientName + "</h3>" + "<p>" + matrix.destination_addresses + "</p>"
   const distances = matrix.rows[0].elements
-  distances.forEach = (distance) => {
+  distances.forEach((distance) => {
     printHTML = printHTML + distance.distance.text + ", time: " + distance.duration.text
-  }
+  })
   return printHTML;
 }
