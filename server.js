115b544bb7bdfcdbac4839f90effad05751b448b
Natalia Stafeeva <n.stafeeva@gmail.com>
2017-05-16 09:58:35 +0100

    Add routes

--- server.js
+++ server.js
@@ -2,10 +2,25 @@ const express = require('express')
 const app = express()
 const fs = require('fs')
 
+
+const candidates = require('./data/candidates.json')
+console.log(candidates.Candidates)
+const clients = require('./data/locations.json')
+console.log(clients.Clients)
+
+
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.send('Homepage...')
+})
+
+app.get('/clients', function (req, res) {
+  res.send('List of clients....')
+})
+
+app.get('/clients/{{clientName}}', function (req, res) {
+  res.send('List of candidates....')
 })
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+  console.log('Go to  localhost:3000!')
 })
