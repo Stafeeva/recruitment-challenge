3b6e16eee7900e61c16b833c9849edadfb7fbe4e
Natalia Stafeeva <n.stafeeva@gmail.com>
2017-05-16 11:46:23 +0100

    Add code to display candidates in a list

--- server.js
+++ server.js
@@ -15,18 +15,23 @@ app.get('/', (req, res) => {
 app.get('/clients', (req, res) => {
   const allClients = clients.Clients
   const printHTML = []
-  allClients.forEach( (name) => {
-    printHTML.push("<h3><a href='/clients/" + name.name + "'>" + name.name + "</a></h3>");
+  allClients.forEach((client) => {
+    printHTML.push("<h3><a href='/clients/" + client.name + "'>" + client.name + "</a></h3>");
   })
   res.send(printHTML.join(''));
-  console.log(printHTML);
 })
 
 app.get('/clients/:clientName', (req, res) => {
   // console.log('List of candidates...')
   // console.log(req.params)
   const clientName = req.params.clientName
-  res.send('a single client: ' + clientName)
+  const allCandidates = candidates.Candidates
+  const printHTML = []
+  allCandidates.forEach((candidate) => {
+    console.log(candidate)
+    printHTML.push("<h4>" + candidate.name + ", postcode:" + candidate.postcode + "</h4>");
+  })
+  res.send('Client name: ' + clientName + printHTML.join(''))
 })
 
 app.listen(3000, () => {
