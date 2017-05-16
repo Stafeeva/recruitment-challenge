74b6755706602bf55748bd657f46ad308930df42
Natalia Stafeeva <n.stafeeva@gmail.com>
2017-05-16 09:48:09 +0100

    Add fs

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
-var express = require('express')
-var app = express()
+const express = require('express')
+const app = express()
+const fs = require('fs')
 
 app.get('/', function (req, res) {
   res.send('Hello World!')
