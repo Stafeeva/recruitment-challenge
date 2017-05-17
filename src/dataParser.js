76a396fc562dd945829cd83d74b4ed5be4435f79
Natalia Stafeeva <n.stafeeva@gmail.com>
2017-05-17 11:44:04 +0100

    Add parseGoogleMatrix function

--- src/dataParser.js
+++ src/dataParser.js
@@ -1,6 +1,6 @@
 module.exports.parseClients = (unparsedClients) => {
 
-  var clientsParsed = {}
+  var clientsParsed = []
 
   unparsedClients.forEach((client) => {
     clientsParsed[client.name] = client.postcode
@@ -18,4 +18,21 @@ module.exports.getPostcodes = (unparsedCandidates) => {
   }
 
   return destinationPostcodes.join("|")
+}
+
+module.exports.parseGoogleMatrix = (unparsedCandidates, matrix) => {
+
+  var parsedGoogleMatrix = []
+
+  for (var i in unparsedCandidates) {
+    var candidate = {}
+    candidate[unparsedCandidates[i].name] = {
+      address : matrix.destination_addresses[i],
+      duration: matrix.rows[0].elements[i].duration.text,
+      distance: matrix.rows[0].elements[i].distance.text
+     }
+    parsedGoogleMatrix.push(candidate)
+  }
+
+  return parsedGoogleMatrix
 }
